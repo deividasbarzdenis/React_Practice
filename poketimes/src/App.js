866058f2772b1b1881994593885a837bceaf4d49
deividasbardzenis <deividasbarzdenis@yahoo.com>
2020-12-29 22:37:08 +0200

    added Browser Router and Link, NavLink

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
@@ -8,6 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
             <NavBar/>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
         </div>
       </BrowserRouter>
     )
